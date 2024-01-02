38ae45f35bec3bce0708284ae2885345e2b28fd9
Bogdan Tsaruk <u.s.a.bogdan@gmail.com>
2024-01-02 22:56:59 +0100

    Add buttons row to ProfileScreen.js.

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -6,6 +6,8 @@ import {
   StyleSheet,
   Text,
 } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 import posts from "../../assets/data/posts.json";
 import FeedPost from "../components/FeedPost";
 import user from "../../assets/data/user.json";
@@ -18,6 +20,34 @@ const ProfileScreen = () => {
         <Image source={{ uri: user.posts[0].image }} style={styles.backImg} />
         <Image source={{ uri: user.image }} style={styles.image} />
         <Text style={styles.name}>{user.name}</Text>
+      </View>
+      {/* Buttons Row */}
+      <View style={styles.buttonsRow}>
+        <Pressable
+          // onPress={onSubmit}
+          style={[styles.button, { backgroundColor: "#2a61ad" }]}
+          // disabled={!postTextState}
+        >
+          <MaterialIcons name="add-circle" size={23} color="white" />
+          <Text style={styles.textActive}>Add to Story</Text>
+        </Pressable>
+
+        <Pressable
+          // onPress={onSubmit}
+          style={[styles.button, { backgroundColor: "lightgray" }]}
+          // disabled={!postTextState}
+        >
+          <MaterialIcons name="edit" size={23} color="black" />
+          <Text style={styles.text}>Edit Profile</Text>
+        </Pressable>
+
+        <Pressable
+          // onPress={onSubmit}
+          style={[styles.button, { backgroundColor: "lightgray" }]}
+          // disabled={!postTextState}
+        >
+          <Ionicons name="md-exit-outline" size={23} color="black" />
+        </Pressable>
       </View>
     </View>
   );
@@ -61,6 +91,41 @@ const styles = StyleSheet.create({
     fontWeight: "500",
     fontSize: 16,
     textAlign: "center",
+  },
+  buttonsRow: {
+    paddingVertical: 14,
+    flexDirection: "row",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: "lightgray",
+    justifyContent: "space-around",
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 17,
+    borderRadius: 5,
+    elevation: 3,
+    marginHorizontal: "auto",
+    display: "flex",
+    flexDirection: "row",
+  },
+  text: {
+    fontSize: 15,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "black",
+    paddingLeft: 4,
+    // textTransform: "uppercase",
+  },
+  textActive: {
+    fontSize: 15,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+    paddingLeft: 4,
   },
 });
 
